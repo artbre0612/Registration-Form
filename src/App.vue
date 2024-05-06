691c330434bde5da5698caf3fd32451c66730760
Arthur <arthur4456645@gmail.com>
2024-05-06 17:25:20 +0800

    add css styling

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,38 @@ import RegistrationForm from './components/RegistrationForm.vue'
 </template>
 
 
-
 <style>
+/* global style setting */
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+TC&display=swap');
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Noto Sans TC", sans-serif;
+  font-weight: 400;
+}
+
+body {
+  background: url('/bg-img.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  backdrop-filter: blur(5px);
+}
+
+#app {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+@media screen and (max-width: 600px) {
+  #app {
+    margin: 25px 0;
+  }
+}
 
 
 </style>
--- src/components/RegistrationForm.vue
+++ src/components/RegistrationForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h1 class="form-title">在這裡註冊</h1>
+    <h1 class="form-title">會員註冊</h1>
     <form>
       <div class="main-user-info">
         
@@ -47,13 +47,143 @@
           <input type="radio" name="pets" id="other">
           <label for="other">其他</label>
         </div>
-        <textarea name="otherPets" id="otherPets" rows="3"placeholder="你養了甚麼特別的寵物?"></textarea>
+        <textarea name="otherPets" id="otherPets" rows="4" cols="25" placeholder="你養了甚麼特別的寵物?"></textarea>
       </div>
 
       <div class="form-submit-btn">
-        <input type="submit" value="送出">
-        <input type="reset" value="重填">
+        <input type="submit" value="註冊" class="submit-btn">
+        <input type="reset" value="重填" class="reset-btn">
       </div>
     </form>
   </div>
 </template>
+
+<style scope>
+  .container {
+    width: 100%;
+    max-width: 650px;
+    padding: 28px;
+    margin: 0 28px;
+    border-radius: 10px;
+    background: rgba(0,0,0,0.5);
+  }
+
+  .form-title {
+    font-size: 35px;
+    font-weight: 600;
+    text-align: center;
+    text-shadow: 1px 1px 1px black;
+    color: white;
+    padding-bottom: 6px;
+    border-bottom: solid 1px white;
+  }
+
+  .main-user-info {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    padding: 20px 0;
+  }
+
+  .user-input-box {
+    display: flex;
+    flex-wrap: wrap;
+    width: 45%;
+    padding-bottom: 15px;
+  }
+
+  .user-input-box:nth-child(2n) {
+    justify-content: end;
+  } 
+
+  .user-input-box label {
+    width: 95%;
+    margin: 5px 0;
+    color: white;
+  }
+
+  .user-input-box input {
+    width: 95%;
+    padding: 5px 10px;
+    border-radius: 8px;
+    border: 0;
+    outline: 0;
+    background-color: #f2f2f2e6
+  }
+
+  .user-details {
+    color: white
+  }
+
+  .pets-category {
+    margin: 15px 0;
+  }
+
+  .pets-category label {
+    padding: 0 20px 0 5px;
+  }
+
+  .pets-category label,
+  .pets-category input,
+  .form-submit-btn input {
+    cursor: pointer;
+  }
+
+  textarea {
+    background-color: #f2f2f2e6;
+    outline: 0;
+    padding: 5px;
+  }
+
+  .form-submit-btn{
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    margin-top: 15px;
+  }
+
+  .form-submit-btn input {
+    width: 48%;
+    font-size: 20px;
+    color: rgb(209, 209, 209);
+    padding: 8px;
+    border: 0;
+  }
+
+  .submit-btn {
+    background-color: rgba(63, 114, 76, 0.7);
+  }
+  .submit-btn:hover {
+    background-color: rgba(50, 121, 68, 0.9);
+  }
+
+  .reset-btn {
+    background-color: rgba(181, 71, 55, 0.7);
+  }
+  .reset-btn:hover {
+    background-color: rgba(181, 71, 55, 0.8);
+  }
+
+  @media screen and (max-width: 600px) {
+    .container {
+      min-width: 280px;
+    }
+
+    .user-input-box {
+      margin-bottom: 12px;
+      width: 100%;
+    }
+
+    .user-input-box:nth-child(2n) {
+      justify-content: space-between;
+    } 
+
+    .pets-category {
+      display: flex;
+      justify-content: space-between;
+      width: 100%;
+    }
+
+  }
+
+</style>
